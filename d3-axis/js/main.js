a995e3243ef4cb4472175a9f21601a5fc0ddfbee
Martin Fabbri <martin.fabbri.a@gmail.com>
2018-04-08 17:13:48 -0700

    Adding labels and legends.

---
+++ d3-axis/js/main.js
@@ -0,0 +1,71 @@
+console.log('Loading json');
+
+const margin = {left: 100, right: 10, top: 10, bottom: 150};
+const width = 600 - margin.left - margin.right;
+const height = 400 - margin.top - margin.bottom;
+
+const svg = d3.select('#chart-area')
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom);
+
+const g = svg.append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+g.append('text')
+    .attr('class', 'x axis-label')
+    .attr('x', width / 2)
+    .attr('y', height + 140)
+    .attr('font-size', '20px')
+    .attr('text-anchor', 'middle')
+    .text(`The world's tallest buildings.`);
+
+
+d3.json('data/buildings.json').then((data) => {
+    data.forEach((d) => {
+       d.age = parseInt(d.age);
+    });
+
+    const x = d3.scaleBand()
+        .domain(data.map((d) => d.name))
+        .range([0, width])
+        .paddingInner(0.3)
+        .paddingOuter(0.3);
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, (data) => data.height)])
+        .range([0, height]);
+
+    const xAxisCall = d3.axisBottom(x);
+    g.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', `translate(0, ${height})`)
+        .call(xAxisCall)
+        .selectAll("text")
+            .attr("y", "10")
+            .attr("x", "10")
+            .attr("text-anchor", "end")
+            .attr("transform", "rotate(-40)");
+
+    const rects = svg.selectAll('rect')
+        .data(data);
+
+
+    const yAxisCall = d3.axisLeft(y)
+        .ticks(3)
+        .tickFormat(d => `${d}m`);
+    g.append('g')
+        .attr('class', 'y axis')
+        .call(yAxisCall);
+
+    rects.enter()
+        .append('rect')
+            .attr('x', (d) => x(d.name))
+            .attr('y', 0)
+            .attr('width', x.bandwidth())
+            .attr('height', (d) => y(d.height));
+
+}).catch(() => {
+    console.log('errorxxx');
+});
+
