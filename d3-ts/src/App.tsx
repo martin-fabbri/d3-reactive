4f36c875c9bf4a2bd076569735275c56e4d46329
Martin Fabbri <martin.fabbri.a@gmail.com>
2018-05-24 13:42:16 -0700

    React D3 Data Graphs - SVG scaling

--- d3-ts/src/App.tsx
+++ d3-ts/src/App.tsx
@@ -2,8 +2,9 @@ import * as React from 'react';
 
 import { Tab, TabId, Tabs } from "@blueprintjs/core";
 
-import BasicsPanel from "./panels/01-basics/basics"
-import SvgPanel from "./panels/02-svg/svg"
+import BasicsPanel from "./panels/01-basics/basics";
+import SvgPanel from "./panels/02-svg/svg";
+import ScalingPanel from "./panels/03-scaling/scaling";
 
 import '@blueprintjs/core/lib/css/blueprint.css';
 import './index.css';
@@ -37,22 +38,14 @@ class App extends React.Component<IProps, IState> {
                     renderActiveTabPanelOnly={this.state.activePanelOnly}
                     vertical={this.state.vertical}
                 >
-                    <Tab id="ng" title="Svg" panel={<SvgPanel />}/>
-                    <Tab id="rx" title="Basics" panel={<BasicsPanel/>} />
-                    <Tab id="mb" title="Ember" panel={<EmberPanel />} />
+                    <Tab id="mb" title="03-Scaling" panel={<ScalingPanel/>} />
+                    <Tab id="ng" title="02-Svg" panel={<SvgPanel />}/>
+                    <Tab id="rx" title="01-Basics" panel={<BasicsPanel/>} />
                     <Tabs.Expander />
                 </Tabs>
             </section>
         );
     }
 }
-
-
-
-const EmberPanel: React.SFC<{}> = () => (
-    <div>
-        <h3>Example 3 ...</h3>
-    </div>
-);
 
 export default App;
--- d3-ts/src/panels/03-scaling/scaling.tsx
+++ d3-ts/src/panels/03-scaling/scaling.tsx
@@ -1,51 +1,33 @@
 import * as React from 'react';
 
-// import {select} from "d3";
+import {scaleLinear} from "d3";
 
 const dataset = [5, 10, 15, 20, 25];
 const height = 300;
-const multiplier = 10;
 const width = 400;
 const barWidth = 20;
 const barPadding = 5;
 const barFillColor = 'teal';
 
+const yScale = scaleLinear()
+    .domain([0, 25])
+    .range([0, height]);
 
-class SvgPanel extends React.Component {
-
-    // public componentDidMount(): void {
-    //
-    //     const svg = select('#d3-container').append('svg');
-    //
-    //     svg
-    //         .attr('width', width)
-    //         .attr('height', height);
-    //
-    //     svg.selectAll('rect')
-    //         .data(dataset)
-    //         .enter()
-    //         .append('rect')
-    //         .attr('x', (d, i) => i * (barWidth + barPadding) )
-    //         .attr('y', d => height - d  * multiplier)
-    //         .attr('width', barWidth)
-    //         .attr('fill', barFillColor )
-    //         .attr('height', d => d * multiplier);
-    // }
-
+class ScalingPanel extends React.Component {
     public render() {
         const barChart = dataset.map((d, i) =>
             <rect
                 key={i}
                 x={i * (barWidth + barPadding)}
-                y={height - d  * multiplier}
+                y={height - yScale(d)}
                 width={barWidth}
                 fill={barFillColor}
-                height={d * multiplier}
+                height={yScale(d)}
             />
         );
         return (
             <div className="mt-4 mb-4">
-                <h3>SVG Basics</h3>
+                <h3>Scaling Basics</h3>
                 <section
                     id='d3-container'
                     className="bg-grey-light"
@@ -60,4 +42,4 @@ class SvgPanel extends React.Component {
     }
 };
 
-export default SvgPanel;
+export default ScalingPanel;
